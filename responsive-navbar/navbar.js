c4cd27d32ea6c30365e3d35f6281d1b462826899
gachez <briangach98@gmail.com>
2019-10-15 19:11:57 +0300

    responsiveness

--- responsive-navbar/navbar.js
+++ responsive-navbar/navbar.js
@@ -1,6 +1,7 @@
 //event to toggle the menu btn
 
 let menuBtn = document.getElementsByClassName('menu-button')[0];
+let bigMenuBtn = document.getElementsByClassName('menu-button')[1]
 let slideMenu = document.getElementsByClassName('slide-menu')[0]
 let slideMenuBtn = document.getElementsByClassName('slide-menu-btn')[0]
 
@@ -16,6 +17,7 @@ const toggleMenuClose = () =>{
  
 }
 
+bigMenuBtn.addEventListener('click', toggleMenu)
 menuBtn.addEventListener('click', toggleMenu)
 slideMenuBtn.addEventListener('click',  // use setTimeout() to execute
   toggleMenuClose
